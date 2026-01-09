72e2ada55542d79b936196d16cb9c0d295d9c5a0
Srdjan <srdjan.suc@hotmail.com>
2026-01-09 21:44:28 +0100

    Fix CORS for POST requests

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -37,6 +37,8 @@ await server.register(helmet, {
 await server.register(cors, {
   origin: true, // Allow all origins for widget embedding
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
 });
 
 await server.register(rateLimit, {
